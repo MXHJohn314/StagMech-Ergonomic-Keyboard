81f5680daa1a74c37c20f4a39c7e4f4165b465be
xXGalacticWaferXx <39640421+xXGalacticWaferXx@users.noreply.github.com>
2020-07-21 14:26:53 -0600

    Update config.h
    now declared SPLIT_HAND_PIN, MATRIX_COL_PINS_RIGHT & MATRIX_ROW_PINS_RIGHT

--- SM_00/config.h
+++ SM_00/config.h
@@ -1,38 +1,29 @@
 #pragma once
-
-/* USB Device descriptor parameter */
+#define MATRIX_ROWS 12
+#define MATRIX_COLS 18
 #define VENDOR_ID       0xA1A1
 #define PRODUCT_ID      0x0001
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    wafer
-#define PRODUCT         SM-00
-#define DESCRIPTION     85% split keyboard
+#define PRODUCT         SM - 00
+#define DESCRIPTION     82-key split keyboard
 
-/* Select hand configuration */
-#define SPLIT_USB_DETECT
-#define MASTER_LEFT
+#define SPLIT_HAND_PIN F0
 #define SOFT_SERIAL_PIN D0
 #define SELECT_SOFT_SERIAL_SPEED 1
+#define MASTER_LEFT
 
-/* key matrix size */
-#define MATRIX_ROWS 12
-#define MATRIX_COLS 8
+#define MATRIX_COL_PINS { D5, F1, F4, F5, F6, F7, B0,B1 }
+#define MATRIX_ROW_PINS { D7, B4, B5, B6, C6, C7  }
 
-/* pro_micro pin-out */
-#define MATRIX_ROW_PINS { D1, D4, C6, D7, E6, B4}
-#define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6, F5, F4 }
+#define MATRIX_COL_PINS_RIGHT { D7, B5, B6, B7, E6, B1, C7, F1 }
+#define MATRIX_ROW_PINS_RIGHT { D3, D4, D5, F5, F1, D5 }
 
-/* leds */
 #define BACKLIGHT_LEVELS 3
-#define BACKLIGHT_PIN B7
+#define BACKLIGHT_PIN F7
 #define BACKLIGHT_BREATHING
 #define BREATHING_PERIOD 5
 
-/* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
